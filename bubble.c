fa1961dd9efa77b680ae67625e6093dcb7a357a0
Steve Maddison <steve@cosam.org>
2011-08-11 15:05:00 +0000

    Initial import
    git-svn-id: http://sources.cosam.org/svn/uzebox/bubble@580 becab9fe-04d6-0310-87a0-b233c6ee48ea

---
+++ bubble.c
@@ -0,0 +1,41 @@
+/*
+ *  A bubbly puzzle game for the Uzebox
+ *  Copyright (C) 2011  Steve Maddison
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdbool.h>
+#include <avr/io.h>
+#include <stdlib.h>
+#include <avr/pgmspace.h>
+#include <uzebox.h>
+
+#include "data/bg.inc"
+#include "data/sprites.inc"
+
+int main(){
+	int i = VRAM_SIZE;
+	SetTileTable(bg_tiles);
+	ClearVram();
+
+	SetTile(0,0,5);
+	SetTile(1,0,6);
+	SetTile(2,0,7);
+	SetTile(3,0,8);
+
+	while(1) {
+	}
+}
+
