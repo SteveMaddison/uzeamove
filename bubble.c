c778dc3dc21584772fce72e7da422679261c8482
Steve Maddison <steve@cosam.org>
2011-08-12 22:16:37 +0000

    Arrow tests.
    git-svn-id: http://sources.cosam.org/svn/uzebox/bubble@587 becab9fe-04d6-0310-87a0-b233c6ee48ea

--- bubble.c
+++ bubble.c
@@ -66,11 +66,35 @@ typedef enum {
 #define FIELD_TILES_V		11
 #define NUM_BUBBLES			83
 
+// Pre-calcutated co-ordinates of arrow parts
+#define ANGLES 30
+const char arrow_x[ANGLES] PROGMEM = {
+	0, 2, 3, 5, 6, 8, 10, 11, 13, 14, 16, 17, 18, 20, 21, 22,
+	23, 24, 25, 26, 27, 28, 28, 29, 29, 30, 30, 31, 31, 31 };
+const char arrow_y[ANGLES] PROGMEM = {
+	31, 31, 31, 31, 30, 30, 29, 29, 28, 28, 27, 26, 25, 24,
+	23, 22, 21, 20, 18, 17, 15, 14, 13, 11, 10, 8, 6, 5, 3, 2 };
+const char ring_x[ANGLES] PROGMEM = {
+	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 13, 14, 15,
+	16, 16, 17, 17, 18, 18, 19, 19, 19, 20, 20, 20, 20 };
+const char ring_y[ANGLES] PROGMEM = {
+	20, 20, 20, 20, 20, 19, 19, 19, 18, 18, 17, 17, 16, 16,
+	15, 14, 13, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+const char rivet_x[ANGLES] PROGMEM = {
+	0, 1, 1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 9, 10, 10, 11,
+	11, 12, 12, 12, 13, 13, 13, 14, 14, 14, 14, 14 };
+const char rivet_y[ANGLES] PROGMEM = {
+	14, 14, 14, 14, 14, 14, 13, 13, 13, 12, 12, 12, 11, 11,
+	10, 10, 9, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 2, 1, 1 };
+
+
 // Globals
 unsigned char bubbles[PLAYERS][NUM_BUBBLES];
 unsigned char current[PLAYERS];
 unsigned char next[PLAYERS];
-
+char angle[PLAYERS];
+#define GEAR_ANIM_STEPS 2
+char gear_anim[PLAYERS];
 
 void draw_field( unsigned char x_pos, unsigned char y_pos, unsigned char player ) {
 	unsigned char x,y,xp,yp,b=0;
@@ -151,6 +175,7 @@ void draw_field( unsigned char x_pos, unsigned char y_pos, unsigned char player 
 
 int main(){
 	SetTileTable(bg_tiles);
+	SetSpritesTileTable(sprite_tiles);
 	ClearVram();
 
 //	bubbles[0][0] = C_RED;
@@ -178,11 +203,65 @@ int main(){
 	draw_field( SCREEN_TILES_H - FIELD_TILES_H - FIELD_OFFSET_X -1, FIELD_OFFSET_Y, 1 );
 	DrawMap2( SCREEN_TILES_H - FIELD_TILES_H - FIELD_OFFSET_X -1, FIELD_OFFSET_Y+FIELD_TILES_V, map_panel );
 
+	sprites[0].tileIndex=15;
+	sprites[1].tileIndex=16;
+	sprites[2].tileIndex=17;
+
 	while(1) {
+		if( ReadJoypad(0) & BTN_LEFT ) {
+			angle[0]--;
+			if( angle[0] < -(ANGLES-1) ) {
+				angle[0] = -(ANGLES-1);
+			}
+			gear_anim[0]--;
+			if( gear_anim[0] < 0 ) {
+				gear_anim[0] = GEAR_ANIM_STEPS-1;
+			}
+		}
+		else if( ReadJoypad(0) & BTN_RIGHT ) {
+			angle[0]++;
+			if( angle[0] > ANGLES-1 ) {
+				angle[0] = ANGLES-1;
+			}
+			gear_anim[0]++;
+			if( gear_anim[0] > GEAR_ANIM_STEPS-1 ) {
+				gear_anim[0] = 0;
+			}
+		}
+
+		if( angle[0] >= 0 ) {
+			sprites[0].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH) + pgm_read_byte( arrow_x + angle[0] ) -4;
+			sprites[1].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH) + pgm_read_byte(  ring_x + angle[0] ) -4;
+			sprites[2].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH) + pgm_read_byte( rivet_x + angle[0] ) -4;
+
+			sprites[0].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte( arrow_y + angle[0] ) -2;
+			sprites[1].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte(  ring_y + angle[0] ) -5;
+			sprites[2].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte( rivet_y + angle[0] ) -3;
+		}
+		else {
+			sprites[0].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH);
+			sprites[0].x -= pgm_read_byte( arrow_x - angle[0] );
+			sprites[0].x -= 4;
+			sprites[1].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH);
+			sprites[1].x -= pgm_read_byte( ring_x - angle[0] );
+			sprites[1].x -= 4;
+			sprites[2].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH);
+			sprites[2].x -= pgm_read_byte( rivet_x - angle[0] );
+			sprites[2].x -= 4;
+
+			sprites[0].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte( arrow_y - angle[0] ) -2;
+			sprites[1].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte(  ring_y - angle[0] ) -5;
+			sprites[2].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte( rivet_y - angle[0] ) -3;
+		}
+		
+		if( gear_anim[0] == 0 ) {
+			DrawMap2( FIELD_OFFSET_X+2, FIELD_OFFSET_Y+FIELD_TILES_V, map_gears1 );
+		}
+		else {
+			DrawMap2( FIELD_OFFSET_X+2, FIELD_OFFSET_Y+FIELD_TILES_V, map_gears2 );
+		}
+
 		WaitVsync(5);
-		DrawMap2( FIELD_OFFSET_X+2, FIELD_OFFSET_Y+FIELD_TILES_V, map_gears2 );
-		WaitVsync(5);
-		DrawMap2( FIELD_OFFSET_X+2, FIELD_OFFSET_Y+FIELD_TILES_V, map_gears1 );
 	}
 }
 
