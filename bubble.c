2513f8b2bb7728f4fd0fd67a9dd20f9a35f2f651
Steve Maddison <steve@cosam.org>
2011-08-13 13:50:34 +0000

    Working arrows for both players.
    git-svn-id: http://sources.cosam.org/svn/uzebox/bubble@588 becab9fe-04d6-0310-87a0-b233c6ee48ea

--- bubble.c
+++ bubble.c
@@ -55,6 +55,9 @@ typedef enum {
 
 #define FIELD_OFFSET_X		2
 #define FIELD_OFFSET_Y		2
+// Offset of player 2's field from that of player 1.
+#define P2_TILE_OFFSET		13
+#define P2_PIXEL_OFFSET		(TILE_WIDTH*P2_TILE_OFFSET)
 
 #include "data/bg.inc"
 #include "data/sprites.inc"
@@ -69,32 +72,45 @@ typedef enum {
 // Pre-calcutated co-ordinates of arrow parts
 #define ANGLES 30
 const char arrow_x[ANGLES] PROGMEM = {
-	0, 2, 3, 5, 6, 8, 10, 11, 13, 14, 16, 17, 18, 20, 21, 22,
-	23, 24, 25, 26, 27, 28, 28, 29, 29, 30, 30, 31, 31, 31 };
+	0, 1, 3, 5, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19,
+	20, 21, 22, 23, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28 };
 const char arrow_y[ANGLES] PROGMEM = {
-	31, 31, 31, 31, 30, 30, 29, 29, 28, 28, 27, 26, 25, 24,
-	23, 22, 21, 20, 18, 17, 15, 14, 13, 11, 10, 8, 6, 5, 3, 2 };
+	31, 31, 31, 31, 31, 30, 30, 30, 29, 29, 28, 28, 26, 26,
+	24, 23, 22, 22, 21, 20, 19, 18, 17, 15, 14, 13, 11, 10, 8, 6 };
+	
 const char ring_x[ANGLES] PROGMEM = {
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 13, 14, 15,
-	16, 16, 17, 17, 18, 18, 19, 19, 19, 20, 20, 20, 20 };
+	 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 13,
+	14, 15, 16, 16, 17, 17, 18, 18, 19, 19, 19, 20, 20, 20, 20 };
 const char ring_y[ANGLES] PROGMEM = {
 	20, 20, 20, 20, 20, 19, 19, 19, 18, 18, 17, 17, 16, 16,
 	15, 14, 13, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
 const char rivet_x[ANGLES] PROGMEM = {
-	0, 1, 1, 2, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 9, 10, 10, 11,
-	11, 12, 12, 12, 13, 13, 13, 14, 14, 14, 14, 14 };
+	 0,  1,  1,  2,  3,  4,  4,  5,  6,  6,  7,  8,  8,  9,  9,
+	10, 10, 11, 11, 12, 12, 12, 13, 13, 13, 14, 14, 14, 14, 14 };
 const char rivet_y[ANGLES] PROGMEM = {
 	14, 14, 14, 14, 14, 14, 13, 13, 13, 12, 12, 12, 11, 11,
 	10, 10, 9, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 2, 1, 1 };
 
+#define TILE_ARROW		23
+#define TILE_RING		15
+#define TILE_RIVET		16
+
+#define SPRITE_ARROW		0
+#define SPRITE_RING			1
+#define SPRITE_RIVET		2
+#define SPRITES_PER_PLAYER	3
 
 // Globals
+unsigned char players = 2;
 unsigned char bubbles[PLAYERS][NUM_BUBBLES];
 unsigned char current[PLAYERS];
 unsigned char next[PLAYERS];
 char angle[PLAYERS];
 #define GEAR_ANIM_STEPS 2
 char gear_anim[PLAYERS];
+unsigned int frame = 0;
+
 
 void draw_field( unsigned char x_pos, unsigned char y_pos, unsigned char player ) {
 	unsigned char x,y,xp,yp,b=0;
@@ -173,7 +189,61 @@ void draw_field( unsigned char x_pos, unsigned char y_pos, unsigned char player 
 	}
 }
 
+bool get_controls( unsigned char player ) {
+	bool changed = false;
+	if( ReadJoypad(player) & BTN_LEFT ) {
+		angle[(int)player]--;
+		if( angle[(int)player] < -(ANGLES-1) ) {
+			angle[(int)player] = -(ANGLES-1);
+		}
+		gear_anim[(int)player]--;
+		if( gear_anim[(int)player] < 0 ) {
+			gear_anim[(int)player] = GEAR_ANIM_STEPS-1;
+		}
+		changed = true;
+	}
+	else if( ReadJoypad(player) & BTN_RIGHT ) {
+		angle[(int)player]++;
+		if( angle[(int)player] > ANGLES-1 ) {
+			angle[(int)player] = ANGLES-1;
+		}
+		gear_anim[(int)player]++;
+		if( gear_anim[(int)player] > GEAR_ANIM_STEPS-1 ) {
+			gear_anim[(int)player] = 0;
+		}
+		changed = true;
+	}
+	return changed;
+}
+
+void draw_arrow( unsigned char x, unsigned char y, unsigned char player ) {
+	unsigned char ps = SPRITES_PER_PLAYER * player;
+	if( angle[player] >= 0 ) {
+		sprites[SPRITE_ARROW+ps].tileIndex = TILE_ARROW + ((angle[player]+2)/5);
+	
+		sprites[SPRITE_ARROW+ps].x = x + pgm_read_byte( arrow_x + angle[player] ) -4;
+		sprites[SPRITE_RING+ps ].x = x + pgm_read_byte(  ring_x + angle[player] ) -4;
+		sprites[SPRITE_RIVET+ps].x = x + pgm_read_byte( rivet_x + angle[player] ) -4;
+
+		sprites[SPRITE_ARROW+ps].y = y - pgm_read_byte( arrow_y + angle[player] ) -2;
+		sprites[SPRITE_RING+ps ].y = y - pgm_read_byte(  ring_y + angle[player] ) -5;
+		sprites[SPRITE_RIVET+ps].y = y - pgm_read_byte( rivet_y + angle[player] ) -3;
+	}
+	else {
+		sprites[SPRITE_ARROW+ps].tileIndex = TILE_ARROW + ((angle[player]-2)/5);
+
+		sprites[SPRITE_ARROW+ps].x = x - pgm_read_byte( arrow_x - angle[player] ) -4;
+		sprites[SPRITE_RING+ps ].x = x - pgm_read_byte(  ring_x - angle[player] ) -4;
+		sprites[SPRITE_RIVET+ps].x = x - pgm_read_byte( rivet_x - angle[player] ) -4;
+
+		sprites[SPRITE_ARROW+ps].y = y - pgm_read_byte( arrow_y - angle[player] ) -2;
+		sprites[SPRITE_RING+ps ].y = y - pgm_read_byte(  ring_y - angle[player] ) -5;
+		sprites[SPRITE_RIVET+ps].y = y - pgm_read_byte( rivet_y - angle[player] ) -3;
+	}
+}
+
 int main(){
+	int p;
 	SetTileTable(bg_tiles);
 	SetSpritesTileTable(sprite_tiles);
 	ClearVram();
@@ -203,65 +273,37 @@ int main(){
 	draw_field( SCREEN_TILES_H - FIELD_TILES_H - FIELD_OFFSET_X -1, FIELD_OFFSET_Y, 1 );
 	DrawMap2( SCREEN_TILES_H - FIELD_TILES_H - FIELD_OFFSET_X -1, FIELD_OFFSET_Y+FIELD_TILES_V, map_panel );
 
-	sprites[0].tileIndex=15;
-	sprites[1].tileIndex=16;
-	sprites[2].tileIndex=17;
+	for( p=0 ; p<PLAYERS ; p++ ) {
+		sprites[SPRITE_ARROW + (p*SPRITES_PER_PLAYER)].tileIndex = TILE_ARROW;
+		sprites[SPRITE_RING  + (p*SPRITES_PER_PLAYER)].tileIndex = TILE_RING;
+		sprites[SPRITE_RIVET + (p*SPRITES_PER_PLAYER)].tileIndex = TILE_RIVET;
+	}
 
 	while(1) {
-		if( ReadJoypad(0) & BTN_LEFT ) {
-			angle[0]--;
-			if( angle[0] < -(ANGLES-1) ) {
-				angle[0] = -(ANGLES-1);
-			}
-			gear_anim[0]--;
-			if( gear_anim[0] < 0 ) {
-				gear_anim[0] = GEAR_ANIM_STEPS-1;
-			}
-		}
-		else if( ReadJoypad(0) & BTN_RIGHT ) {
-			angle[0]++;
-			if( angle[0] > ANGLES-1 ) {
-				angle[0] = ANGLES-1;
-			}
-			gear_anim[0]++;
-			if( gear_anim[0] > GEAR_ANIM_STEPS-1 ) {
-				gear_anim[0] = 0;
+		for( p=0 ; p < players ; p++ ) {
+			if( get_controls(p) || frame == 0 ) {
+				if( players == 1 ) {
+					draw_arrow(
+						(SCREEN_TILES_H/2) * TILE_WIDTH,
+						((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT), p );
+				}
+				else {
+					draw_arrow(
+						((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH) + (P2_PIXEL_OFFSET*p),
+						(FIELD_OFFSET_Y + FIELD_TILES_H) * TILE_HEIGHT, p );
+				}
+				
+				if( gear_anim[p] == 0 ) {
+					DrawMap2( FIELD_OFFSET_X+2 + (P2_TILE_OFFSET*p), FIELD_OFFSET_Y+FIELD_TILES_V, map_gears1 );
+				}
+				else {
+					DrawMap2( FIELD_OFFSET_X+2 + (P2_TILE_OFFSET*p), FIELD_OFFSET_Y+FIELD_TILES_V, map_gears2 );
+				}
 			}
 		}
 
-		if( angle[0] >= 0 ) {
-			sprites[0].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH) + pgm_read_byte( arrow_x + angle[0] ) -4;
-			sprites[1].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH) + pgm_read_byte(  ring_x + angle[0] ) -4;
-			sprites[2].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH) + pgm_read_byte( rivet_x + angle[0] ) -4;
-
-			sprites[0].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte( arrow_y + angle[0] ) -2;
-			sprites[1].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte(  ring_y + angle[0] ) -5;
-			sprites[2].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte( rivet_y + angle[0] ) -3;
-		}
-		else {
-			sprites[0].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH);
-			sprites[0].x -= pgm_read_byte( arrow_x - angle[0] );
-			sprites[0].x -= 4;
-			sprites[1].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH);
-			sprites[1].x -= pgm_read_byte( ring_x - angle[0] );
-			sprites[1].x -= 4;
-			sprites[2].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH);
-			sprites[2].x -= pgm_read_byte( rivet_x - angle[0] );
-			sprites[2].x -= 4;
-
-			sprites[0].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte( arrow_y - angle[0] ) -2;
-			sprites[1].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte(  ring_y - angle[0] ) -5;
-			sprites[2].y = ((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT) - pgm_read_byte( rivet_y - angle[0] ) -3;
-		}
-		
-		if( gear_anim[0] == 0 ) {
-			DrawMap2( FIELD_OFFSET_X+2, FIELD_OFFSET_Y+FIELD_TILES_V, map_gears1 );
-		}
-		else {
-			DrawMap2( FIELD_OFFSET_X+2, FIELD_OFFSET_Y+FIELD_TILES_V, map_gears2 );
-		}
-
-		WaitVsync(5);
+		frame++;
+		WaitVsync(3);
 	}
 }
 
