01a232b2d48b94b8edeb8ee3ce09b40d561e7400
Steve Maddison <steve@cosam.org>
2011-08-13 19:13:07 +0000

    Bubble trajectories and bouncing.
    git-svn-id: http://sources.cosam.org/svn/uzebox/bubble@589 becab9fe-04d6-0310-87a0-b233c6ee48ea

--- bubble.c
+++ bubble.c
@@ -48,11 +48,14 @@ typedef enum {
 #define BUBBLE_ODD_MIDDLE			10
 #define BUBBLE_ODD_L				11
 #define BUBBLE_ODD_R				12
+#define BUBBLE_ODD_L_BLANK			13
+#define BUBBLE_ODD_R_BLANK			14
 // Number of bubble tiles of each colour
-#define BUBBLES_PER_COLOUR			13
+#define BUBBLES_PER_COLOUR			15
 #define BUBBLE_SLIVER_L				9
 #define BUBBLE_SLIVER_R				10
 
+#define BUBBLE_WIDTH		12
 #define FIELD_OFFSET_X		2
 #define FIELD_OFFSET_Y		2
 // Offset of player 2's field from that of player 1.
@@ -62,7 +65,6 @@ typedef enum {
 #include "data/bg.inc"
 #include "data/sprites.inc"
 
-#define PLAYERS				2
 #define FIELD_BUBBLES_H		8
 #define FIELD_BUBBLES_V		11
 #define FIELD_TILES_H		12
@@ -72,41 +74,77 @@ typedef enum {
 // Pre-calcutated co-ordinates of arrow parts
 #define ANGLES 30
 const char arrow_x[ANGLES] PROGMEM = {
-	0, 1, 3, 5, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19,
-	20, 21, 22, 23, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28 };
+	 0,  1,  3,  5,  7,  8,  9, 10, 12, 13, 14, 15, 16, 17, 18,
+	19, 20, 21, 22, 23, 24, 24, 25, 25, 26, 26, 27, 27, 28, 28 };
 const char arrow_y[ANGLES] PROGMEM = {
-	31, 31, 31, 31, 31, 30, 30, 30, 29, 29, 28, 28, 26, 26,
-	24, 23, 22, 22, 21, 20, 19, 18, 17, 15, 14, 13, 11, 10, 8, 6 };
+	31, 31, 31, 31, 31, 30, 30, 30, 29, 29, 28, 28, 26, 26,	24,
+	23, 22, 22, 21, 20, 19, 18, 17, 15, 14, 13, 11, 10,  8,  6 };
 	
 const char ring_x[ANGLES] PROGMEM = {
 	 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 13,
 	14, 15, 16, 16, 17, 17, 18, 18, 19, 19, 19, 20, 20, 20, 20 };
 const char ring_y[ANGLES] PROGMEM = {
-	20, 20, 20, 20, 20, 19, 19, 19, 18, 18, 17, 17, 16, 16,
-	15, 14, 13, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+	20, 20, 20, 20, 20, 19, 19, 19, 18, 18, 17, 17, 16, 16, 15,
+	14, 13, 13, 12, 11, 10,  9,  8,  7,  6,  5,  4,  3,  2,  1 };
 
 const char rivet_x[ANGLES] PROGMEM = {
 	 0,  1,  1,  2,  3,  4,  4,  5,  6,  6,  7,  8,  8,  9,  9,
 	10, 10, 11, 11, 12, 12, 12, 13, 13, 13, 14, 14, 14, 14, 14 };
 const char rivet_y[ANGLES] PROGMEM = {
-	14, 14, 14, 14, 14, 14, 13, 13, 13, 12, 12, 12, 11, 11,
-	10, 10, 9, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 2, 1, 1 };
+	14, 14, 14, 14, 14, 14, 13, 13, 13, 12, 12, 12, 11, 11, 10,
+	10,  9,  9,  8,  8,  7,  6,  6,  5,  4,  4,  3,  2,  1,  1 };
 
+// Same for projectile paths, but mutiplied by a trajectory factor.
+#define TRAJ_FACTOR 8
+const char traj_x[ANGLES] PROGMEM = {
+	 0,  3,  5,  8, 10, 12, 15, 17, 20, 22, 24, 26, 28, 30, 32,
+	34, 36, 37, 39, 40, 42, 43, 44, 45, 46, 46, 47, 47, 48, 48 };
+const char traj_y[ANGLES] PROGMEM = {
+	48, 48, 48, 47, 47, 46, 46, 45, 44, 43, 42, 40, 39, 37, 36,
+	34, 32, 30, 28, 26, 24, 22, 20, 17, 15, 12, 10,  8,  5,  3 };
+
+
+// Sprite constants/macros
 #define TILE_ARROW		23
 #define TILE_RING		15
 #define TILE_RIVET		16
 
+#define TILE_BUBBLE_L(c)	((c*2)-1)
+#define TILE_BUBBLE_R(c)	(c*2)
+
 #define SPRITE_ARROW		0
 #define SPRITE_RING			1
 #define SPRITE_RIVET		2
-#define SPRITES_PER_PLAYER	3
+
+#define SPRITE_CURRENT_L	3
+#define SPRITE_CURRENT_R	4
+#define SPRITE_PROJ_L		5
+#define SPRITE_PROJ_R		6
+#define SPRITE_NEXT_L		7
+#define SPRITE_NEXT_R		8
+
+#define SPRITES_PER_PLAYER	9
+
+// Structures
+typedef struct {
+	char c; // Colour
+	char angle;
+	int x;
+	int y;
+} projectile_t;
+
 
 // Globals
-unsigned char players = 2;
+#define PLAYERS 1
+unsigned char players = 1;
 unsigned char bubbles[PLAYERS][NUM_BUBBLES];
 unsigned char current[PLAYERS];
 unsigned char next[PLAYERS];
 char angle[PLAYERS];
+projectile_t proj[PLAYERS];
+bool block_fire[PLAYERS];
+unsigned char block_left[PLAYERS];
+unsigned char block_right[PLAYERS];
 #define GEAR_ANIM_STEPS 2
 char gear_anim[PLAYERS];
 unsigned int frame = 0;
@@ -136,13 +174,8 @@ void draw_field( unsigned char x_pos, unsigned char y_pos, unsigned char player 
 							SetTile( xp, yp, BUBBLE_FIRST_TILE + bubbles[player][b+1] + 1 );
 						}
 						else {
-							if( b == 0 ) {
-								SetTile( xp, yp, BUBBLE_FIRST_COLOUR_TILE + bubbles[player][b+1] + BUBBLE_EVEN_R_SPLIT );
-							}
-							else {
-								SetTile( xp, yp, BUBBLE_FIRST_COLOUR_TILE + (BUBBLES_PER_COLOUR*(bubbles[player][b]-1))
-									+ bubbles[player][b+1] + BUBBLE_EVEN_R_SPLIT );
-							}
+							SetTile( xp, yp, BUBBLE_FIRST_COLOUR_TILE + (BUBBLES_PER_COLOUR*(bubbles[player][b]-1))
+								+ bubbles[player][b+1] + BUBBLE_EVEN_R_SPLIT );
 						}
 						b++;
 						break;
@@ -165,6 +198,9 @@ void draw_field( unsigned char x_pos, unsigned char y_pos, unsigned char player 
 						else if( bubbles[player][b] != C_BLANK ) {
 							SetTile( xp, yp, BUBBLE_FIRST_COLOUR_TILE + (BUBBLES_PER_COLOUR*(bubbles[player][b-1]-1)) + BUBBLE_ODD_R );
 						}
+						else if( bubbles[player][b-1] != C_BLANK ) {
+							SetTile( xp, yp, BUBBLE_FIRST_COLOUR_TILE + (BUBBLES_PER_COLOUR*(bubbles[player][b-1]-1)) + BUBBLE_ODD_R_BLANK );
+						}
 						break;
 					case 1:
 						if( bubbles[player][b] != C_BLANK ) {
@@ -176,8 +212,11 @@ void draw_field( unsigned char x_pos, unsigned char y_pos, unsigned char player 
 						if( ( x == FIELD_TILES_H-1 || bubbles[player][b] == C_BLANK) && bubbles[player][b-1] != C_BLANK ) {
 							SetTile( xp, yp, BUBBLE_SLIVER_R );
 						}
-						else {
-							if( bubbles[player][b] != C_BLANK ) {
+						else if( bubbles[player][b] != C_BLANK ) {
+							if( bubbles[player][b-1] == C_BLANK ) {
+								SetTile( xp, yp, BUBBLE_FIRST_COLOUR_TILE + (BUBBLES_PER_COLOUR*(bubbles[player][b]-1)) + BUBBLE_ODD_L_BLANK );
+							}
+							else {
 								SetTile( xp, yp, BUBBLE_FIRST_COLOUR_TILE + (BUBBLES_PER_COLOUR*(bubbles[player][b]-1)) + BUBBLE_ODD_L );
 							}
 						}
@@ -189,29 +228,85 @@ void draw_field( unsigned char x_pos, unsigned char y_pos, unsigned char player 
 	}
 }
 
-bool get_controls( unsigned char player ) {
+void new_bubble( unsigned char player ) {
+	unsigned char ps = (player*SPRITES_PER_PLAYER);
+	
+	current[player] = next[player];
+	next[player] = (random()%(C_COUNT-1)) + 1;
+	
+	sprites[SPRITE_CURRENT_L+ps].tileIndex = TILE_BUBBLE_L( current[(int)player] );
+	sprites[SPRITE_CURRENT_R+ps].tileIndex = TILE_BUBBLE_R( current[(int)player] );
+
+	sprites[SPRITE_NEXT_L+ps].tileIndex = TILE_BUBBLE_L( next[(int)player] );
+	sprites[SPRITE_NEXT_R+ps].tileIndex = TILE_BUBBLE_R( next[(int)player] );
+}
+
+bool proc_controls( unsigned char player ) {
 	bool changed = false;
+	
+	if( block_left[player]  ) block_left[player]--;
+	if( block_right[player] ) block_right[player]--;
+	
 	if( ReadJoypad(player) & BTN_LEFT ) {
-		angle[(int)player]--;
-		if( angle[(int)player] < -(ANGLES-1) ) {
-			angle[(int)player] = -(ANGLES-1);
+		if( !block_left[player] ) {
+			// Rotate left
+			angle[(int)player]--;
+			if( angle[(int)player] < -(ANGLES-1) ) {
+				angle[(int)player] = -(ANGLES-1);
+			}
+			gear_anim[(int)player]--;
+			if( gear_anim[(int)player] < 0 ) {
+				gear_anim[(int)player] = GEAR_ANIM_STEPS-1;
+			}
+			changed = true;
+			block_left[player] = 3;
+			block_right[player] = 0;
 		}
-		gear_anim[(int)player]--;
-		if( gear_anim[(int)player] < 0 ) {
-			gear_anim[(int)player] = GEAR_ANIM_STEPS-1;
-		}
-		changed = true;
 	}
-	else if( ReadJoypad(player) & BTN_RIGHT ) {
-		angle[(int)player]++;
-		if( angle[(int)player] > ANGLES-1 ) {
-			angle[(int)player] = ANGLES-1;
+	else {
+		block_left[player] = 0;
+	}
+	
+	if( ReadJoypad(player) & BTN_RIGHT ) {
+		if( !block_right[player] ) {
+			// Rotate right
+			angle[(int)player]++;
+			if( angle[(int)player] > ANGLES-1 ) {
+				angle[(int)player] = ANGLES-1;
+			}
+			gear_anim[(int)player]++;
+			if( gear_anim[(int)player] > GEAR_ANIM_STEPS-1 ) {
+				gear_anim[(int)player] = 0;
+			}
+			changed = true;
+			block_right[player] = 3;
+			block_left[player] = 0;
 		}
-		gear_anim[(int)player]++;
-		if( gear_anim[(int)player] > GEAR_ANIM_STEPS-1 ) {
-			gear_anim[(int)player] = 0;
+	}
+	else {
+		block_right[player] = 0;
+	}
+	
+	if( ReadJoypad(player) & (BTN_A|BTN_B|BTN_X|BTN_Y) ) {
+		if( !block_fire[player] ) {
+			// Fire!
+			unsigned char ps = (player*SPRITES_PER_PLAYER);
+
+			proj[player].x = ((FIELD_TILES_H*TILE_WIDTH)/2) - (BUBBLE_WIDTH/2);
+			proj[player].y = ((FIELD_TILES_V+1)*TILE_HEIGHT) - (BUBBLE_WIDTH/2);
+			
+			proj[player].x *= TRAJ_FACTOR;
+			proj[player].y *= TRAJ_FACTOR;
+			
+			proj[player].c = current[player];
+			proj[player].angle = angle[player];
+		
+			sprites[SPRITE_PROJ_L+ps].tileIndex = TILE_BUBBLE_L(proj[player].c);
+			sprites[SPRITE_PROJ_R+ps].tileIndex = TILE_BUBBLE_R(proj[player].c);
+		
+			new_bubble(player);
 		}
-		changed = true;
+		block_fire[player] = true;
 	}
 	return changed;
 }
@@ -242,68 +337,157 @@ void draw_arrow( unsigned char x, unsigned char y, unsigned char player ) {
 	}
 }
 
+void update_projectile( unsigned char player ) {
+	unsigned char ps = (player*SPRITES_PER_PLAYER);
+
+	proj[player].y -= pgm_read_byte( traj_y + proj[player].angle );
+	
+	if( proj[player].angle >= 0 ) {
+		proj[player].x += pgm_read_byte( traj_x + proj[player].angle );
+		if( proj[player].x >= ((FIELD_TILES_H*TILE_WIDTH)-BUBBLE_WIDTH) * TRAJ_FACTOR ) {
+			// Handle bounce. Until we're back on the board, step back
+			// through the last step we made. This calls for increased
+			// precision...
+			proj[player].x *= TRAJ_FACTOR;
+			proj[player].y *= TRAJ_FACTOR;
+			
+			while( proj[player].x >= ((FIELD_TILES_H*TILE_WIDTH)-BUBBLE_WIDTH) * (TRAJ_FACTOR*TRAJ_FACTOR) ) {
+				proj[player].x -= pgm_read_byte( traj_x + proj[player].angle );
+				proj[player].y += pgm_read_byte( traj_y + proj[player].angle );
+			}
+
+			proj[player].x /= TRAJ_FACTOR;
+			proj[player].y /= TRAJ_FACTOR;
+			proj[player].angle = -proj[player].angle;
+		}
+	}
+	else {
+		proj[player].x -= pgm_read_byte( traj_x - proj[player].angle );
+		while( proj[player].x < 0 ) {
+			// Handle bounce
+			proj[player].x *= TRAJ_FACTOR;
+			proj[player].y *= TRAJ_FACTOR;
+			
+			while( proj[player].x < 0 ) {
+				proj[player].x += pgm_read_byte( traj_x + proj[player].angle );
+				proj[player].y += pgm_read_byte( traj_y + proj[player].angle );
+			}
+
+			proj[player].x /= TRAJ_FACTOR;
+			proj[player].y /= TRAJ_FACTOR;
+			proj[player].angle = -proj[player].angle;
+		}
+	}
+
+	if( proj[player].y < 0 ) {
+		proj[player].c = C_BLANK;
+		sprites[SPRITE_PROJ_L+ps].tileIndex = 0;
+		sprites[SPRITE_PROJ_R+ps].tileIndex = 0;
+		block_fire[player] = false;
+	}
+	else {	
+		if( players == 1 ) {
+			sprites[SPRITE_PROJ_L+ps].x = ((SCREEN_TILES_H-FIELD_TILES_H)/2 * TILE_WIDTH) + (proj[player].x/TRAJ_FACTOR);
+		}
+		else {
+			sprites[SPRITE_PROJ_L+ps].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH)
+				+ (P2_PIXEL_OFFSET*player) + (proj[player].x/TRAJ_FACTOR);
+		}
+		sprites[SPRITE_PROJ_R+ps].x = sprites[SPRITE_PROJ_L+ps].x + TILE_WIDTH;
+	
+		sprites[SPRITE_PROJ_L+ps].y = (FIELD_OFFSET_Y*TILE_HEIGHT) + (proj[player].y/TRAJ_FACTOR);
+		sprites[SPRITE_PROJ_R+ps].y = sprites[SPRITE_PROJ_L+ps].y;
+	}
+}
+
 int main(){
 	int p;
 	SetTileTable(bg_tiles);
 	SetSpritesTileTable(sprite_tiles);
 	ClearVram();
 
-//	bubbles[0][0] = C_RED;
-	bubbles[0][1] = C_ORANGE;
-	bubbles[0][2] = C_BLANK;
-	bubbles[0][3] = C_GREEN;
-	bubbles[0][4] = C_BLUE;
-	bubbles[0][5] = C_BLANK;
-	bubbles[0][6] = C_BLACK;
-	bubbles[0][7] = C_RED;
-
-	bubbles[0][8] = C_RED;
-	bubbles[0][9] = C_BLANK;
-	bubbles[0][10] = C_YELLOW;
-//	bubbles[0][11] = C_GREEN;
-	bubbles[0][12] = C_BLUE;
-	bubbles[0][13] = C_PURPLE;
-	bubbles[0][14] = C_BLACK;
-	
-	bubbles[0][15] = C_RED;
-
-	draw_field( FIELD_OFFSET_X, FIELD_OFFSET_Y, 0 );
-	DrawMap2( FIELD_OFFSET_X, FIELD_OFFSET_Y+FIELD_TILES_V, map_panel );
-	
-	draw_field( SCREEN_TILES_H - FIELD_TILES_H - FIELD_OFFSET_X -1, FIELD_OFFSET_Y, 1 );
-	DrawMap2( SCREEN_TILES_H - FIELD_TILES_H - FIELD_OFFSET_X -1, FIELD_OFFSET_Y+FIELD_TILES_V, map_panel );
+	for( p = 0 ; p < 16 ; p++ ) {
+		bubbles[0][p] = random()%C_COUNT;
+	}
 
 	for( p=0 ; p<PLAYERS ; p++ ) {
-		sprites[SPRITE_ARROW + (p*SPRITES_PER_PLAYER)].tileIndex = TILE_ARROW;
-		sprites[SPRITE_RING  + (p*SPRITES_PER_PLAYER)].tileIndex = TILE_RING;
-		sprites[SPRITE_RIVET + (p*SPRITES_PER_PLAYER)].tileIndex = TILE_RIVET;
+		unsigned char ps = (p*SPRITES_PER_PLAYER);
+		
+		if( players == 1 ) {
+			draw_field( (SCREEN_TILES_H-FIELD_TILES_H)/2, FIELD_OFFSET_Y, p );
+			DrawMap2( (SCREEN_TILES_H-FIELD_TILES_H)/2, FIELD_OFFSET_Y+FIELD_TILES_V, map_panel );
+		}
+		else {
+			draw_field( FIELD_OFFSET_X, FIELD_OFFSET_Y, p );
+			DrawMap2( FIELD_OFFSET_X, FIELD_OFFSET_Y+FIELD_TILES_V, map_panel );
+	
+			draw_field( SCREEN_TILES_H - FIELD_TILES_H - FIELD_OFFSET_X -1, FIELD_OFFSET_Y, 1 );
+			DrawMap2( SCREEN_TILES_H - FIELD_TILES_H - FIELD_OFFSET_X -1, FIELD_OFFSET_Y+FIELD_TILES_V, map_panel );
+		}
+	
+		// Tile indeces of arrow parts.
+		sprites[SPRITE_ARROW+ps].tileIndex = TILE_ARROW;
+		sprites[SPRITE_RING +ps].tileIndex = TILE_RING;
+		sprites[SPRITE_RIVET+ps].tileIndex = TILE_RIVET;
+
+		// Position and indeces of current/next bubbles.
+		if( players == 1 ) {
+			sprites[SPRITE_CURRENT_L+ps].x = ((SCREEN_TILES_H/2) * TILE_WIDTH) - (BUBBLE_WIDTH/2);
+		}
+		else {
+			sprites[SPRITE_CURRENT_L+ps].x = ((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH) + (P2_PIXEL_OFFSET*p) - (BUBBLE_WIDTH/2);
+		}
+		
+		sprites[SPRITE_CURRENT_L+ps].y = ((FIELD_OFFSET_Y+FIELD_TILES_V)*TILE_HEIGHT) + (BUBBLE_WIDTH/2);
+		sprites[SPRITE_NEXT_L+ps].x = sprites[SPRITE_CURRENT_L+ps].x + 40;
+		sprites[SPRITE_NEXT_L+ps].y = sprites[SPRITE_CURRENT_L+ps].y + 1;
+		
+		sprites[SPRITE_CURRENT_R+ps].x = sprites[SPRITE_CURRENT_L+ps].x + TILE_WIDTH;
+		sprites[SPRITE_CURRENT_R+ps].y = sprites[SPRITE_CURRENT_L+ps].y;
+		sprites[SPRITE_NEXT_R+ps].x = sprites[SPRITE_NEXT_L+ps].x + TILE_WIDTH;
+		sprites[SPRITE_NEXT_R+ps].y = sprites[SPRITE_NEXT_L+ps].y;
+		
+		new_bubble(p); // Initialise next
+		new_bubble(p); // Initialise current and next
+		proj[p].c = C_BLANK;
 	}
 
 	while(1) {
+		WaitVsync(1);
+
 		for( p=0 ; p < players ; p++ ) {
-			if( get_controls(p) || frame == 0 ) {
+			if( proc_controls(p) || frame == 0 ) {
 				if( players == 1 ) {
 					draw_arrow(
 						(SCREEN_TILES_H/2) * TILE_WIDTH,
 						((FIELD_OFFSET_Y + FIELD_TILES_H)*TILE_HEIGHT), p );
+
+					if( gear_anim[p] == 0 ) {
+						DrawMap2( ((SCREEN_TILES_H-FIELD_TILES_H)/2)+2 , FIELD_OFFSET_Y+FIELD_TILES_V, map_gears1 );
+					}
+					else {
+						DrawMap2( ((SCREEN_TILES_H-FIELD_TILES_H)/2)+2, FIELD_OFFSET_Y+FIELD_TILES_V, map_gears2 );
+					}
 				}
 				else {
 					draw_arrow(
 						((FIELD_OFFSET_X + (FIELD_TILES_H/2))*TILE_WIDTH) + (P2_PIXEL_OFFSET*p),
 						(FIELD_OFFSET_Y + FIELD_TILES_H) * TILE_HEIGHT, p );
+
+					if( gear_anim[p] == 0 ) {
+						DrawMap2( FIELD_OFFSET_X+2 + (P2_TILE_OFFSET*p), FIELD_OFFSET_Y+FIELD_TILES_V, map_gears1 );
+					}
+					else {
+						DrawMap2( FIELD_OFFSET_X+2 + (P2_TILE_OFFSET*p), FIELD_OFFSET_Y+FIELD_TILES_V, map_gears2 );
+					}
 				}
-				
-				if( gear_anim[p] == 0 ) {
-					DrawMap2( FIELD_OFFSET_X+2 + (P2_TILE_OFFSET*p), FIELD_OFFSET_Y+FIELD_TILES_V, map_gears1 );
-				}
-				else {
-					DrawMap2( FIELD_OFFSET_X+2 + (P2_TILE_OFFSET*p), FIELD_OFFSET_Y+FIELD_TILES_V, map_gears2 );
-				}
+			}
+			if( proj[p].c != C_BLANK ) {
+				update_projectile(p);
 			}
 		}
 
 		frame++;
-		WaitVsync(3);
 	}
 }
 
